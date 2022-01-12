130df88236042e0e646610e461d19620641d054a
jaca347 <jaca347@gmail.com>
2022-01-12 13:12:33 +0100

    Removing unecessary imports

--- git3Client/gitCommands/branch.py
+++ git3Client/gitCommands/branch.py
@@ -1,13 +1,6 @@
-import os, operator
-
 from git3Client.exceptions.NoRepositoryError import NoRepositoryError
 
-from git3Client.gitInternals.fileMode import GIT_NORMAL_FILE_MODE
-from git3Client.gitInternals.gitIndex import read_index, write_index
-from git3Client.gitInternals.gitObject import hash_object
-from git3Client.gitInternals.IndexEntry import IndexEntry
-
-from git3Client.utils.utils import list_files_in_dir, get_repo_root_path, read_file
+from git3Client.utils.utils import list_files_in_dir, get_repo_root_path
 
 def branch():
     """Add all file paths to git index."""
