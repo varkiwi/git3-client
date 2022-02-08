d4c25c821f39887f3e4ea612c9cd6fcdf8ae076a
jaca347 <jaca347@gmail.com>
2022-02-08 12:02:43 +0100

    Fixing push

--- git3Client/gitCommands/push.py
+++ git3Client/gitCommands/push.py
@@ -5,7 +5,7 @@ from git3Client.dlt.storageClient import getStorageClient
 from git3Client.utils.utils import get_active_branch_hash, get_current_branch_name
 
 def push():
-    """Push master branch to given git repo URL.""" 
+    """Push current active branch to given git repo URL.""" 
     if not check_if_repo_created():
         print('Repository has not been registered yet. Use\n\n`git3 create`\n\nbefore you push')
         return
@@ -13,16 +13,18 @@ def push():
     active_branch_name = get_current_branch_name()
 
     local_sha1 = get_active_branch_hash()
-    remote_cid = get_remote_branch_hash(active_branch_name)
+    remote_database_cid = get_remote_branch_hash(active_branch_name)
 
     client = getStorageClient()
     # if remote_cid is none, nothing has been pushed yet.
-    if remote_cid != None:
+    if remote_database_cid != None:
         # since there is already something pushed, we will have to get the remote cid
-        remote_database = client.get_json(remote_cid)
-        remote_commit = client.get_json(remote_database['head_cid'])
+        remote_database = client.get_json(remote_database_cid)
+        remote_commit_cid = remote_database['head_cid']
+        remote_commit = client.get_json(remote_commit_cid)
         remote_sha1 = remote_commit['sha1']
     else:
+        remote_commit_cid = None
         remote_sha1 = None
         # is going to contain all data in order to make loading the directory structure faster!
         remote_database = {
@@ -39,15 +41,15 @@ def push():
        return
 
     print('Pushing files to IPFS')
-    master_cid = push_commit(local_sha1, remote_sha1, remote_cid, remote_database)
-    remote_database['head_cid'] = master_cid
+    branch_cid = push_commit(local_sha1, remote_sha1, remote_commit_cid, remote_database)
+    remote_database['head_cid'] = branch_cid
 
-    if master_cid == remote_cid:
+    if branch_cid == remote_database_cid:
         print('Everything up-to-date')
     else:
         del remote_database['path']
         del remote_database['committer']
         del remote_database['currentCommitMessage']
-        master_cid = client.add_json(remote_database)
+        branch_cid = client.add_json(remote_database)
         print('Going to write the CID into repository contract')
-        push_new_cid(active_branch_name, master_cid)
+        push_new_cid(active_branch_name, branch_cid)
