25613e2c0c01e85844dbc375d44444a4c77b7333
jaca347 <jaca347@gmail.com>
2022-02-24 14:47:07 +0100

    Adding updated config

--- git3Client/config/config.py
+++ git3Client/config/config.py
@@ -2,8 +2,10 @@
 
 CHAINID = 80001
 GIT_FACTORY_ADDRESS = '0x38FF86F13806F4Fb5A15Ba3558Ff257875D2acd8'
-RPC_ADDRESS = 'https://rpc-mumbai.maticvigil.com/v1/f632570838c8d7c5e5c508c6f24a0e23eabac8c7'
+#RPC_ADDRESS = 'https://rpc-mumbai.maticvigil.com/v1/f632570838c8d7c5e5c508c6f24a0e23eabac8c7'
+RPC_ADDRESS = 'https://matic-mumbai.chainstacklabs.com'
 IPFS_CONNECTION = '/dns4/ipfs.infura.io/tcp/5001/https'
+# IPFS_CONNECTION = '/ip4/127.0.0.1/tcp/5001'
 MUMBAI_GAS_STATION='https://gasstation-mumbai.matic.today'
 
 # POLYGON MAINNET
