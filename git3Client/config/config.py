cb759dfd88cd981316b49b3ebb5da969c4ee0640
jacekv <jaca347@gmail.com>
2022-05-30 10:28:50 +0200

    issue-10: Update Godwoken config

--- git3Client/config/config.py
+++ git3Client/config/config.py
@@ -7,18 +7,10 @@ MUMBAI_RPC_ADDRESS = 'https://matic-mumbai.chainstacklabs.com'
 MUMBAI_GAS_STATION='https://gasstation-mumbai.matic.today'
 
 # NERVOS GODWOKEN TESTNET
-GODWOKEN_TESTNET_CHAINID = 868455272153094
-GODWOKEN_TESTNET_GIT_FACTORY_ADDRESS = '0xc6289F7fa0dA125B2FFA932c9A655a99a5427073'
-GODWOKEN_TESTNET_RPC_ADDRESS = 'https://godwoken-testnet-web3-v1-rpc.ckbapp.dev'
+GODWOKEN_TESTNET_CHAINID = 71401
+GODWOKEN_TESTNET_GIT_FACTORY_ADDRESS = '0x9639eB95Ae2Ba325027006fb2D81f667e3ebFE18'
+GODWOKEN_TESTNET_RPC_ADDRESS = 'https://godwoken-testnet-v1.ckbapp.dev'
 # GODWOKEN_TESTNET_GAS_STATION='https://gasstation-mumbai.matic.today'
 
-CHAINID = 80001
-GIT_FACTORY_ADDRESS = '0x38FF86F13806F4Fb5A15Ba3558Ff257875D2acd8'
-#RPC_ADDRESS = 'https://rpc-mumbai.maticvigil.com/v1/f632570838c8d7c5e5c508c6f24a0e23eabac8c7'
-RPC_ADDRESS = 'https://matic-mumbai.chainstacklabs.com'
 IPFS_CONNECTION = '/dns4/ipfs.infura.io/tcp/5001/https'
-# IPFS_CONNECTION = '/ip4/127.0.0.1/tcp/5001'
-MUMBAI_GAS_STATION='https://gasstation-mumbai.matic.today'
 
-# POLYGON MAINNET
-# CHAINID = 137
